1998f2eee43e9fd05f5d1ce01ee32554a06195c9
Trevis Thomas <trevislynnthomas@gmail.com>
2018-06-14 19:29:22 -0600

    First Commit

---
+++ 01-Activities/01-HelloNode/hello.js
@@ -0,0 +1,10 @@
+// Run this program by navigating to it in terminal/bash.
+// Then run node Hello.js.
+console.log("Hello");
+
+for (i = 0; i < 10; i++) {
+
+    var mood = "so tired"
+    console.log("I am " + mood);
+
+}
